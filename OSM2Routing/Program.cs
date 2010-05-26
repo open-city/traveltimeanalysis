880ede9eb6426d4d45c30399821fc198c55b8fe9
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-05-26 13:16:20 +0000

    First version of OSM2Routing project
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@19 a27b73b0-2c29-76c0-2b42-66a06cb7688c

---
+++ OSM2Routing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NDesk.Options;
+
+namespace LK.OSM2Routing {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
