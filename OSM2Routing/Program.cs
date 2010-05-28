4a6821582e60adf4cea25c3b892b8e05720f2734
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-05-28 11:45:15 +0000

    Small test fixes OSM2Routing - parsing command line parameters; code to do the actual job
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@28 a27b73b0-2c29-76c0-2b42-66a06cb7688c

--- OSM2Routing/Program.cs
+++ OSM2Routing/Program.cs
@@ -4,24 +4,69 @@ using System.Linq;
 using System.Text;
 
 using NDesk.Options;
+
 using LK.OSMUtils.OSMDatabase;
 
 namespace LK.OSM2Routing {
 	class Program {
 		static void Main(string[] args) {
-			RoadType acceptedRoad = new RoadType();
-			acceptedRoad.RequiredTags.Add(new OSMTag("highway", "*"));
+			string configPath = "";
+			string osmPath = "";
+			string outputPath = "";
+			bool showHelp = false;
 
-			DateTime start = DateTime.Now;
+			OptionSet parameters = new OptionSet() {
+				{ "osm=",					v => osmPath = v},
+				{ "c|config=",			v => configPath = v},
+				{ "o|output=",			v => outputPath = v},
+				{ "h|?|help",				v => showHelp = v != null},
+			};
 
-			OSMRoutingDB target = new OSMRoutingDB();
-			target.Load(new RoadType[] { acceptedRoad }, "C:\\temp\\czech_republic.osm");
+			try {
+				parameters.Parse(args);
+			}
+			catch (OptionException e) {
+				Console.Write("OSM2Routing: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `osm2routing --help' for more information.");
+				return;
+			}
 
-			OSMDB routable = target.BuildRoutableOSM();
-			routable.Save("C:\\temp\\test-segments.osm");
+			if (showHelp) {
+				ShowHelp(parameters);
+				return;
+			}
 
-			Console.WriteLine(DateTime.Now - start);
-			Console.WriteLine("Done.");
+			Console.Write("Loading config file ...");		
+			RoutingConfig config = new RoutingConfig();
+			config.Load(configPath);
+			Console.WriteLine("\t\t\tDone.");
+
+			Console.Write("Loading OSM file ...");
+			OSMRoutingDB map = new OSMRoutingDB();
+			map.Load(config.RoadTypes, osmPath);
+			Console.WriteLine("\t\t\tDone.");
+
+			Console.Write("Creating routable OSM file ...");
+			OSMDB routableMap = map.BuildRoutableOSM();
+			Console.WriteLine("\t\tDone.");
+
+			Console.Write("Saving routable OSM file ...");
+			routableMap.Save(outputPath);
+			Console.WriteLine("\t\tDone.");
+		}
+
+		/// <summary>
+		/// Prints a help message
+		/// </summary>
+		/// <param name="p">The parameters accepted by this program</param>
+		static void ShowHelp(OptionSet p) {
+			Console.WriteLine("Usage: osm2routing [OPTIONS]+");
+			Console.WriteLine("Converts OSM file into routing-friendly form");
+			Console.WriteLine();
+
+			Console.WriteLine("Options:");
+			p.WriteOptionDescriptions(Console.Out);
 		}
 	}
 }
