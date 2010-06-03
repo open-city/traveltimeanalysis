6c507ca16ad42e462c9995b370d3a86748c3a638
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-06-03 12:33:07 +0000

    MatchGPX2OSM - parameters parsing
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@39 a27b73b0-2c29-76c0-2b42-66a06cb7688c

--- MatchGPX2OSM/Program.cs
+++ MatchGPX2OSM/Program.cs
@@ -3,9 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using NDesk.Options;
+
 namespace MatchGPX2OSM {
 	class Program {
 		static void Main(string[] args) {
+			string osmPath = "";
+			string gpxPath = "";
+			string outputPath = "";
+			bool showHelp = false;
+
+			OptionSet parameters = new OptionSet() {
+				{ "osm=",					v => osmPath = v},
+				{ "gpx=",					v => gpxPath = v},
+				{ "o|output=",			v => outputPath = v},
+				{ "h|?|help",				v => showHelp = v != null},
+			};
+
+			try {
+				parameters.Parse(args);
+			}
+			catch (OptionException e) {
+				Console.Write("MatchGPX2OSM: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `matchgpx2osm --help' for more information.");
+				return;
+			}
+
+
+			if (showHelp) {
+				ShowHelp(parameters);
+				return;
+			}
+
+
+		}
+
+		/// <summary>
+		/// Prints a help message
+		/// </summary>
+		/// <param name="p">The parameters accepted by this program</param>
+		static void ShowHelp(OptionSet p) {
+			Console.WriteLine("Usage: matchgpx2osm [OPTIONS]+");
+			Console.WriteLine("Matches GPX track to the OSM map");
+			Console.WriteLine();
+
+			Console.WriteLine("Options:");
+			p.WriteOptionDescriptions(Console.Out);
 		}
 	}
 }
