f08fcadb29aca06c390d411f487840afcd7e52b9
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-06-18 08:21:38 +0000

    Adds GPXUtils/Filters/FrequncyFilter.cs
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@62 a27b73b0-2c29-76c0-2b42-66a06cb7688c

---
+++ GPXUtils/Filters/FrequncyFilter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LK.GPXUtils;
+
+namespace LK.GPXUtils.Filters {
+	/// <summary>
+	/// Implements filter that reduces frequncy of the GPS track
+	/// </summary>
+	public class FrequncyFilter {
+		public GPXTrackSegment Filter(TimeSpan minInterval, GPXTrackSegment track) {
+			GPXTrackSegment result = new GPXTrackSegment();
+
+			if (track.Nodes.Count == 0)
+				return result;
+
+			GPXPoint last = track.Nodes[0];
+			result.Nodes.Add(last);
+
+			int index = 1;
+			while (index < track.Nodes.Count) {
+				if (track.Nodes[index].Time - last.Time >= minInterval) {
+					last = track.Nodes[index];
+					result.Nodes.Add(last);
+				}
+				index++;
+			}
+			return result;
+		}
+	}
+}
