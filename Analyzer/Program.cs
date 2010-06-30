9931fd435e6dfa78528f28d4beade0f86bf8105a
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-06-30 11:37:52 +0000

    Analyzer - creates initial project structure
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@101 a27b73b0-2c29-76c0-2b42-66a06cb7688c

---
+++ Analyzer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LK.Analyzer {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
