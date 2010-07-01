bbe2c78d19e58f4d19a3d91647c3abafd2fb3e52
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-07-01 21:11:57 +0000

    Analyzer - adds XmlTravelTimeDB class and ITravelTimesDB interface
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@104 a27b73b0-2c29-76c0-2b42-66a06cb7688c

---
+++ Analyzer/ITravelTimesDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LK.Analyzer {
+	public interface ITravelTimesDB {
+		IEnumerable<TravelTime> TravelTimes { get; }
+		IEnumerable<TravelTime> GetTravelTimes(SegmentInfo segment);
+
+		void AddTravelTime(TravelTime toAdd);
+		bool RemoveTravelTime(TravelTime toRemove);
+	}
+}
---
+++ Analyzer/XmlTravelTimeDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LK.Analyzer {
+	public class XmlTravelTimeDB : ITravelTimesDB {
+		Dictionary<SegmentInfo, List<TravelTime>> _storage;
+
+		public IEnumerable<TravelTime> TravelTimes {
+			get {
+				return _storage.Values.SelectMany(segTravelTimes => segTravelTimes);
+			}
+		}
+
+		public IEnumerable<TravelTime> GetTravelTimes(SegmentInfo segment) {
+			return _storage[segment];
+		}
+
+		public void AddTravelTime(TravelTime toAdd) {
+			if (_storage.ContainsKey(toAdd.Segment) == false) {
+				_storage.Add(toAdd.Segment, new List<TravelTime>());
+			}
+
+			_storage[toAdd.Segment].Add(toAdd);
+		}
+
+		public bool RemoveTravelTime(TravelTime toRemove) {
+			if (_storage.ContainsKey(toRemove.Segment)) {
+				return _storage[toRemove.Segment].Remove(toRemove);
+			}
+
+			return false;
+		}
+
+		public XmlTravelTimeDB() {
+			_storage = new Dictionary<SegmentInfo, List<TravelTime>>();
+		}
+	}
+}
