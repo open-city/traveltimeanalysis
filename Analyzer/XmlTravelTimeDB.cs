7a03f7644a64e4cf1008a258af116f9ce58dea3c
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-07-02 04:18:59 +0000

    Analyzer/XmlTravelTimeDB - adds support for loading from the XML file
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@105 a27b73b0-2c29-76c0-2b42-66a06cb7688c

--- Analyzer/XmlTravelTimeDB.cs
+++ Analyzer/XmlTravelTimeDB.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.IO;
+
+using LK.GPXUtils;
 
 namespace LK.Analyzer {
 	public class XmlTravelTimeDB : ITravelTimesDB {
 		Dictionary<SegmentInfo, List<TravelTime>> _storage;
+		XmlReader _xmlReader;
 
 		public IEnumerable<TravelTime> TravelTimes {
 			get {
@@ -35,6 +40,178 @@ namespace LK.Analyzer {
 
 		public XmlTravelTimeDB() {
 			_storage = new Dictionary<SegmentInfo, List<TravelTime>>();
+		}
+
+		public void Load(string filename) {
+			using(FileStream fs = new FileStream(filename, FileMode.Open)) {
+				Load(fs);
+			}
+		}
+
+		public void Load(Stream stream) {
+			XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+			xmlReaderSettings.IgnoreComments = true;
+			xmlReaderSettings.IgnoreProcessingInstructions = true;
+			xmlReaderSettings.IgnoreWhitespace = true;
+
+			try {
+				_xmlReader = XmlTextReader.Create(stream, xmlReaderSettings);
+
+				_xmlReader.Read();
+				while (false == _xmlReader.EOF) {
+
+
+					switch (_xmlReader.NodeType) {
+						case XmlNodeType.XmlDeclaration:
+							_xmlReader.Read();
+							continue;
+
+						case XmlNodeType.Element:
+							if (_xmlReader.Name != "travel-time-db")
+								throw new XmlException("Invalid xml root element. Expected <travel-time-db>.");
+
+							ReadRootTag();
+							return;
+
+						default:
+							throw new XmlException();
+					}
+				}
+			}
+			finally {
+				_xmlReader.Close();
+				_xmlReader = null;
+			}
+		}
+
+		/// <summary>
+		/// Reads the root element of the xml
+		/// </summary>
+		private void ReadRootTag() {
+			_xmlReader.Read();
+
+			while (_xmlReader.NodeType != XmlNodeType.EndElement) {
+				switch (_xmlReader.Name) {
+					case "segment":
+						ReadSegment();
+						break;
+					default:
+						_xmlReader.Skip();
+						break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Reads an OSMTag from the XmlTextReader and reurn it.
+		/// </summary>
+		/// <returns>The OSMTag read form the XmlTextReader</returns>
+		private void ReadSegment() {
+			SegmentInfo segment = new SegmentInfo();
+
+			string attFrom = _xmlReader.GetAttribute("from");
+			if (attFrom == null)
+				throw new XmlException("Attribute 'from' is missing.");
+			segment.NodeFromID = int.Parse(attFrom);
+
+			string attTo = _xmlReader.GetAttribute("to");
+			if (attTo == null)
+				throw new XmlException("Attribute 'to' is missing.");
+			segment.NodeToID = int.Parse(attTo);
+
+			string attWay = _xmlReader.GetAttribute("way");
+			if (attFrom == null)
+				throw new XmlException("Attribute 'way' is missing.");
+			segment.WayID = int.Parse(attWay);
+
+			_storage.Add(segment, new List<TravelTime>());
+
+			if (false == _xmlReader.IsEmptyElement) {
+				_xmlReader.Read();
+
+				while (_xmlReader.NodeType != XmlNodeType.EndElement) {
+					switch (_xmlReader.NodeType) {
+						case XmlNodeType.Element:
+							switch (_xmlReader.Name) {
+								case "travel-time":
+									_storage[segment].Add(ReadTravelTime(segment));
+									continue;
+								default:
+									_xmlReader.Skip();
+									continue;
+							}
+						default:
+							_xmlReader.Skip();
+							break;
+					}
+				}
+			}
+
+			_xmlReader.Skip();
+		}
+
+		private TravelTime ReadTravelTime(SegmentInfo segment) {
+			string attStart = _xmlReader.GetAttribute("start");
+			if (attStart == null)
+				throw new XmlException("Attribute 'start' is missing.");
+			DateTime start = DateTime.Parse(attStart);
+
+			string attEnd = _xmlReader.GetAttribute("end");
+			if (attStart == null)
+				throw new XmlException("Attribute 'end' is missing.");
+			DateTime end = DateTime.Parse(attEnd);
+
+			List<GPXPoint> points = new List<GPXPoint>();
+
+			if (false == _xmlReader.IsEmptyElement) {
+				_xmlReader.Read();
+
+				while (_xmlReader.NodeType != XmlNodeType.EndElement) {
+					switch (_xmlReader.NodeType) {
+						case XmlNodeType.Element:
+							switch (_xmlReader.Name) {
+								case "pt":
+									points.Add(ReadPoint());
+									continue;
+								default:
+									_xmlReader.Skip();
+									continue;
+							}
+						default:
+							_xmlReader.Skip();
+							break;
+					}
+				}
+			}
+
+			_xmlReader.Skip();
+
+			return new TravelTime(segment, start, end, points);
+		}
+
+		private GPXPoint ReadPoint() {
+			string lat = _xmlReader.GetAttribute("lat");
+			if (string.IsNullOrEmpty(lat)) {
+				throw new XmlException("Attribute 'lat' is missing.");
+			}
+			double pointLat = double.Parse(lat, System.Globalization.CultureInfo.InvariantCulture);
+
+
+			string lon = _xmlReader.GetAttribute("lon");
+			if (string.IsNullOrEmpty(lon)) {
+				throw new XmlException("Attribute 'lon' is missing.");
+			}
+			double pointLon = double.Parse(lon, System.Globalization.CultureInfo.InvariantCulture);
+
+			string timeAtt = _xmlReader.GetAttribute("time");
+			if (string.IsNullOrEmpty(timeAtt)) {
+				throw new XmlException("Attribute 'lon' is missing.");
+			}
+			DateTime time = DateTime.Parse(timeAtt);
+
+			_xmlReader.Skip();
+
+			return new GPXPoint(pointLat, pointLon, time);
 		}
 	}
 }
