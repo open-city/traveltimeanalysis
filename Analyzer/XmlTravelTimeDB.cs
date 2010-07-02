15d4b561f045e95d6a3767ad3e7e612e8cbd992d
lukas@kabrt.cz <lukas@kabrt.cz@a27b73b0-2c29-76c0-2b42-66a06cb7688c>
2010-07-02 05:46:18 +0000

    Analyzer/XmlTravelTimeDB - adds support for saving to XML
    git-svn-id: http://traveltimeanalysis.googlecode.com/svn/trunk@106 a27b73b0-2c29-76c0-2b42-66a06cb7688c

--- Analyzer/XmlTravelTimeDB.cs
+++ Analyzer/XmlTravelTimeDB.cs
@@ -11,6 +11,7 @@ namespace LK.Analyzer {
 	public class XmlTravelTimeDB : ITravelTimesDB {
 		Dictionary<SegmentInfo, List<TravelTime>> _storage;
 		XmlReader _xmlReader;
+		XmlWriter _xmlWriter;
 
 		public IEnumerable<TravelTime> TravelTimes {
 			get {
@@ -42,6 +43,63 @@ namespace LK.Analyzer {
 			_storage = new Dictionary<SegmentInfo, List<TravelTime>>();
 		}
 
+		public void Save(string filename) {
+			using (FileStream fs = new FileStream(filename, FileMode.Create)) {
+				Save(fs);
+			}
+		}
+
+		public void Save(Stream stream) {
+			XmlWriterSettings writerSetting = new XmlWriterSettings();
+			writerSetting.Indent = true;
+
+			_xmlWriter = XmlTextWriter.Create(new StreamWriter(stream, new UTF8Encoding(false)), writerSetting);
+
+			_xmlWriter.WriteStartElement("travel-time-db");
+
+			foreach (var segment in _storage.Keys) {
+				WriteSegment(segment);
+			}
+
+			_xmlWriter.WriteEndElement();
+			_xmlWriter.Close();
+		}
+
+		void WriteSegment(SegmentInfo segment) {
+			_xmlWriter.WriteStartElement("segment");
+			_xmlWriter.WriteAttributeString("from", segment.NodeFromID.ToString());
+			_xmlWriter.WriteAttributeString("to", segment.NodeToID.ToString());
+			_xmlWriter.WriteAttributeString("way", segment.WayID.ToString());
+
+			foreach (var traveltime in _storage[segment]) {
+				WriteTravelTime(traveltime);
+			}
+
+			_xmlWriter.WriteEndElement();
+		}
+
+		void WriteTravelTime(TravelTime tt) {
+			_xmlWriter.WriteStartElement("travel-time");
+			_xmlWriter.WriteAttributeString("start", tt.TimeStart.ToString());
+			_xmlWriter.WriteAttributeString("end", tt.TimeEnd.ToString());
+			
+			foreach (var point in tt.Points) {
+				WritePoint(point);
+			}
+
+			_xmlWriter.WriteEndElement();
+		}
+
+		void WritePoint(GPXPoint point) {
+			_xmlWriter.WriteStartElement("travel-time");
+
+			_xmlWriter.WriteAttributeString("lat", point.Latitude.ToString(System.Globalization.CultureInfo.InvariantCulture));
+			_xmlWriter.WriteAttributeString("lon", point.Longitude.ToString(System.Globalization.CultureInfo.InvariantCulture));
+			_xmlWriter.WriteAttributeString("time", point.Time.ToString());
+
+			_xmlWriter.WriteEndElement();		
+		}
+		
 		public void Load(string filename) {
 			using(FileStream fs = new FileStream(filename, FileMode.Open)) {
 				Load(fs);
